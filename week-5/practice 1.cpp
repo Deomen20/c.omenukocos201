90e444ffee1d05eb4c8812eafa20f2c8dabaabcc
Deomen20 <chidera.omenuko@pau.edu.ng>
2025-11-30 21:55:42 +0100

    My fifth commit

---
+++ week-5/practice 1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int size = 5;
+    int arr[size] = {1,3,4,5,6};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout<< size<<endl;
+    for (int i=0 ; i<= size -1; i++){
+        cout<<arr[i]<<"\n";
+    }
+    
+    
+    // int sum = arr[3] + arr[4];
+    // cout<< sum<<endl;
+    // for (int i=0 ; i<= size -1; i++){
+    //     cout<<arr[i]<<"\n";
+    // }
+
+// for (int i=0 ; i<= size -1; i++){
+//         cout<<arr[i]<<"\n";
+
+
+    // int array1[] ={1,3,4,5,6};
+    // int array1[] = {1,3,4,5,6};
+    // int array1[5] = {1,3,4,5,6};
+    // int array1[5] = {1,3,4,5,6};
+
+
+    // cout << arr[0];
+    // cout << arr[1];
+    // cout << arr[2];
+    // cout << arr[3];
+    // cout << arr[4];
+
+}
